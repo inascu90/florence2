6b1ffa1b2c91984c0ac56a79015cecebec8f3fa1
inas <inascu90@gmail.com>
2024-10-27 21:53:37 +0300

    flor.py modified v2

--- flor.py
+++ flor.py
@@ -49,7 +49,7 @@ def run_example(task_prompt, image, text_input=None):
 cam_url = "lifting.mp4"
 cam_url_path = f"videos/{cam_url}"
 cap = cv2.VideoCapture(cam_url_path)
-
+#####
 i = 0
 start_time = time.time()
 
